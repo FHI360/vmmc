d063d4b50eb92a43018172e2c5f9911a419d337d
root <root@techlab360.org>
2014-05-14 15:27:46 -0400

    Adding ignored files

---
+++ sites/default/files/js_injector/js_injector_2.js
@@ -0,0 +1,5 @@
+jQuery("#forum-topic-list table").removeClass();
+jQuery("#forum-topic-list table").addClass("table table-hover table-bordered table-striped");
+
+jQuery("#forum-topic-list table td").removeClass('active');
+

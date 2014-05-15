8d8a6830027b0dd4e698dfea227a0036c003feda
root <root@techlab360.org>
2014-05-15 11:42:16 -0400

    removing files folder. Again

--- sites/default/files/js_injector/js_injector_2.js
+++
@@ -1,5 +0,0 @@
-jQuery("#forum-topic-list table").removeClass();
-jQuery("#forum-topic-list table").addClass("table table-hover table-bordered table-striped");
-
-jQuery("#forum-topic-list table td").removeClass('active');
-

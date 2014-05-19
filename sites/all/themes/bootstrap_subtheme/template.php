3167a32764184a445375141746df8fbaad2055c8
root <root@techlab360.org>
2014-05-19 11:13:14 -0400

    Modified template.php to disable login form autocomplete

--- sites/all/themes/bootstrap_subtheme/template.php
+++ sites/all/themes/bootstrap_subtheme/template.php
@@ -8,12 +8,16 @@ function bootstrap_subtheme_form_alter(&$form, &$form_state, $form_id) {
   switch ($form_id) {
     case 'user_login_block': // Log-in block form.
     case 'user_login': // Log-in form.
-    case 'user_register': // Log-in form.
+//    case 'user_register_form': // Register form.
+//    case 'user_profile_form': // Register form.
       // Username
-      $form['name']['#attributes']['autocomplete'] = 'off';
+      //$form['name']['#attributes']['autocomplete'] = 'off';
+      $form['#attributes']['autocomplete'] = 'off';
 
       // Password
       $form['pass']['#attributes']['autocomplete'] = 'off';
+ //     $form['pass']['pass1']['#attributes']['autocomplete'] = 'off';
+ //     $form['pass[pass2]']['#attributes']['autocomplete'] = 'off';
       break;
   }
 }
